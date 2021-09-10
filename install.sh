a35ff240ac68cf514c2aff73886f303f90fe12cb
crimera <stevendavemiranda2@gmail.com>
2021-09-10 11:30:26 +0800

    Added the ability to make clips

--- install.sh
+++ install.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 mode=$1
+GREEN='\033[1;31m'
 
 inst () {
   cp -r $1 ~/
 }
 
 try_install() {
-    # dpkg -l "$1" | grep -q ^ii && return 1
-    # pkg upgrade
+    dpkg -l "$1" | grep -q ^ii && return 1
+    pkg upgrade
     # apt-get -y install "$@"
+    pkg install $@
+    echo "${GREEN}$1 is installed"
     return 0
 }
 
+packages () {
+    try_install python
+    try_install zsh
+    try_install git
+    try_install jq
+    try_install termux-api
+}
+
 addpackages () {
-    try_install python zsh git
+    packages
+    
     if [ $mode == up ]; then
-        echo "..."
+        echo "Updating"
     else
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         pip install yt-dlp
@@ -27,3 +39,5 @@ inst bin
 inst .termux
 
 addpackages
+
+termux-setup-storage
