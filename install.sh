24b04db51bac79b1f30e4ee68c08a5d23a69b25d
crimera <stevendavemiranda2@gmail.com>
2021-09-10 08:59:06 +0800

    Added the up option ASMR is now saved to "storage/shared/ASMR/%(channel)s/%(upload_date)s-%(title)s-%(id)s.m4a"

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+mode=$1
+
 inst () {
   cp -r $1 ~/
 }
 
+try_install() {
+    # dpkg -l "$1" | grep -q ^ii && return 1
+    # pkg upgrade
+    # apt-get -y install "$@"
+    return 0
+}
+
 addpackages () {
-  pkg upgrade
-  pkg install python zsh git
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  python3 -m pip install --upgrade pip
-  pip install yt-dlp
+    try_install python zsh git
+    if [ $mode == up ]; then
+        echo "..."
+    else
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        pip install yt-dlp
+    fi
 }
 
 inst bin
