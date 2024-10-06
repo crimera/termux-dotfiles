421d69751e7ba2d63e621e3127bd92f7e1af0d2f
crimera <44558091+crimera@users.noreply.github.com>
2024-10-06 00:50:26 +0000

    neovim: use neotree, use ruff format

--- neovim/.config/nvim/install.sh
+++
@@ -1,2 +0,0 @@
-pkg install clang zig zls taplo lua-language-server stylua binutils-is-llvm ripgrep fd rust rust-analyzer golang ruff
-pip install -y pyright
--- neovim/.config/nvim/lua/plugins/nvimtree.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"nvim-tree/nvim-tree.lua",
-	config = function()
-		require("nvim-tree").setup()
-		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-	end,
-}
