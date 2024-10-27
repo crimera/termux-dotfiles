bc37185c6a2251617ffc0b12b0218d8dbc228702
crimera <44558091+crimera@users.noreply.github.com>
2024-10-27 11:59:36 +0800

    neovim: add copilot chat

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
-	"github/copilot.vim"
+	"CopilotC-Nvim/CopilotChat.nvim",
+	branch = "canary",
+	dependencies = {
+		{ "github/copilot.vim" }, -- or github/copilot.vim
+		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	},
+	-- build = "make tiktoken", -- Only on MacOS or Linux
+	opts = {
+		debug = true, -- Enable debugging
+	},
 }
