14ef75c76627355311cf4cabe45bb428cf671f44
crimera <44558091+crimera@users.noreply.github.com>
2024-11-04 13:30:02 +0800

    neovim: use ctrl+e for neotree

--- neovim/.config/nvim/lua/plugins/neotree.lua
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -11,13 +11,13 @@ return {
 	},
 	cmd = "Neotree",
 	keys = {
-		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+		{ "<C-e>", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
 	},
 	opts = {
 		filesystem = {
 			window = {
 				mappings = {
-					["\\"] = "close_window",
+					["<C-e>"] = "close_window",
 				},
 			},
 		},
