be5090aeee4c0f11b606f2f038699a7600c6c1bc
crimera <44558091+crimera@users.noreply.github.com>
2024-11-04 13:32:17 +0800

    neovim: move setup to config

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,17 +3,15 @@ return { -- Highlight, edit, and navigate code
 
 	build = ":TSUpdate",
 	commit = "8012b55eee65eba1d1ee4df0a186d30e72dcbe65",
-	opts = {
-		ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
-		-- Autoinstall languages that are not installed
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-			--  If you are experiencing weird indenting issues, add the language to
-			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "bash", "c", "lua", "markdown", "vim", "vimdoc"},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			autopairs = { enable = true },
+			incremental_selection = { enable = true },
+			context_commentstring = { enable = true },
+		})
+	end,
 }
