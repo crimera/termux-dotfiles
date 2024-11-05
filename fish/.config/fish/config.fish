574754aaebe4d37019f0af25edae8365c0a16c90
crimera <stevendavemiranda2@gmail.com>
2024-11-05 07:37:09 +0000

    fix(fish): wrong if statement

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,12 +4,12 @@ end
 
 fish_add_path "$HOME/bin"
 
-if ! command -v pacman > /dev/null
+if command -v pacman > /dev/null
 	alias xi="sudo pacman -S"
 	alias xr="sudo pacman -R --recursive"
 else
-	alias xi="xbps-install -Su"
-	alias xr="xbps-remove -Ro"
+	alias xi="sudo xbps-install -Su"
+	alias xr="sudo xbps-remove -Ro"
 end
 
 fish_add_path "~/.cargo/bin"
@@ -18,3 +18,7 @@ fish_add_path "~/.local/bin"
 if command -v getprop > /dev/null
 	fish_add_path "~/bin" # termux binaries
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
