1143115512e1283cb5fb07701b559d16954ca2d2
crimera <44558091+crimera@users.noreply.github.com>
2024-11-07 14:53:27 +0800

    neovim: separate cmp config

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -2,12 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
 		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
-		"saadparwaiz1/cmp_luasnip",
-		"L3MON4D3/LuaSnip",
-
 		-- Useful status updates for LSP.
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 		{ "j-hui/fidget.nvim", opts = {} },
@@ -132,59 +126,6 @@ return {
 		lspconfig.zls.setup({
 			settings = {
 				enable_build_on_save = false,
-			},
-		})
-
-		-- luasnip setup
-		local luasnip = require("luasnip")
-
-		-- nvim-cmp setup
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			selection = {
-				behavior = cmp.SelectBehavior.Select,
-				auto_select = true,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-				["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-				-- C-b (back) C-f (forward) for snippet placeholder navigation.
-
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({
-
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
 			},
 		})
 	end,
